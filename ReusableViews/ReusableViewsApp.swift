5bb7131393becd74e3de947e42d04779a35317ff
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-09-10 08:00:26 +0200

    Initial Commit

---
+++ ReusableViews/ReusableViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableViewsApp.swift
+//  ReusableViews
+//
+//  Created by Lynda Chiwetelu on 10.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ReusableViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
